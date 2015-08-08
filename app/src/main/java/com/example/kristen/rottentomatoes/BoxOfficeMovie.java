7f70e38671de9423da1fd888d9583e40e70402b7
Kristen Sundquist <kssundquist@gmail.com>
2015-08-08 12:27:40 -0700

    Add factory method for deserializing JSON response

--- app/src/main/java/com/example/kristen/rottentomatoes/BoxOfficeMovie.java
+++ app/src/main/java/com/example/kristen/rottentomatoes/BoxOfficeMovie.java
@@ -2,6 +2,10 @@ package com.example.kristen.rottentomatoes;
 
 import android.text.TextUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 /**
@@ -37,5 +41,28 @@ public class BoxOfficeMovie {
 
     public String getCastList() {
         return TextUtils.join(", ", castList);
+    }
+
+    public static BoxOfficeMovie fromJson(JSONObject jsonObject) {
+        BoxOfficeMovie b = new BoxOfficeMovie();
+        try {
+            b.title = jsonObject.getString("title");
+            b.year = jsonObject.getInt("year");
+            b.synopsis = jsonObject.getString("synopsis");
+            b.posterUrl = jsonObject.getJSONObject("posters").getString("thumbnail");
+            b.criticsScore = jsonObject.getJSONObject("ratings").getInt("critics_score");
+
+            b.castList = new ArrayList<>();
+            JSONArray abridgedCast = jsonObject.getJSONArray("abridged_cast");
+            for (int i = 0; i < abridgedCast.length(); i++) {
+                b.castList.add(abridgedCast.getJSONObject(i).getString("name"));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return b;
     }
 }
