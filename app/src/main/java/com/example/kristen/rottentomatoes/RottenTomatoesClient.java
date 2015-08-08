5b974ada7709ddf2fe0700c5da43fdfab3f8464e
Kristen Sundquist <kssundquist@gmail.com>
2015-08-08 12:22:27 -0700

    Add RottenTomatoesClient

---
+++ app/src/main/java/com/example/kristen/rottentomatoes/RottenTomatoesClient.java
@@ -0,0 +1,28 @@
+package com.example.kristen.rottentomatoes;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by kristen on 8/8/15.
+ */
+public class RottenTomatoesClient {
+    private final String API_KEY = "95wsptzedh7yfpfx4r537zvu";
+    private final String API_BASE_URL = "http://api.rottentomatoes.com/api/public/v1.0/";
+    private AsyncHttpClient client;
+
+    public RottenTomatoesClient() {
+        this. client = new AsyncHttpClient();
+    }
+
+    private String getApiUrl(String relativeUrl) {
+        return API_BASE_URL + relativeUrl;
+    }
+
+    public void getBoxOfficeMovies(JsonHttpResponseHandler handler) {
+        String url = getApiUrl("lists/movies/box_office.json");
+        RequestParams params = new RequestParams("apikey", API_KEY);
+        client.get(url, params, handler);
+    }
+}
