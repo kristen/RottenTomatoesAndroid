95efcfeea34b73f45cdfa9bfe2b935d8969d4e8e
Kristen Sundquist <kssundquist@gmail.com>
2015-08-08 14:38:18 -0700

    Populate list view with rotten tomatoes JSON

--- app/src/main/java/com/example/kristen/rottentomatoes/BoxOfficeActivity.java
+++ app/src/main/java/com/example/kristen/rottentomatoes/BoxOfficeActivity.java
@@ -1,16 +1,57 @@
 package com.example.kristen.rottentomatoes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
-public class BoxOfficeActivity extends AppCompatActivity {
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class BoxOfficeActivity extends Activity {
+    private ListView lvMovies;
+    private BoxOfficeMoviesAdapter adapterMovies;
+    RottenTomatoesClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_box_office);
+
+        lvMovies = (ListView) findViewById(R.id.lvMovies);
+        ArrayList<BoxOfficeMovie> aMovies = new ArrayList<>();
+        adapterMovies = new BoxOfficeMoviesAdapter(this, aMovies);
+        lvMovies.setAdapter(adapterMovies);
+
+        fetchBoxOfficeMovies();
+    }
+
+    private void fetchBoxOfficeMovies() {
+        client = new RottenTomatoesClient();
+        client.getBoxOfficeMovies(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject responseBody) {
+                JSONArray items = null;
+                try {
+                    items = responseBody.getJSONArray("movies");
+                    ArrayList<BoxOfficeMovie> movies = BoxOfficeMovie.fromJson(items);
+
+                    for (BoxOfficeMovie movie : movies) {
+                        adapterMovies.add(movie);
+                    }
+                    adapterMovies.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
