3acbdc3dd9a797c7d2585a7fa1411e5eb09f6f81
Kristen Sundquist <kssundquist@gmail.com>
2015-08-08 14:37:28 -0700

    Create BoxOfficeMoviesAdapter to populate view for movies

---
+++ app/src/main/java/com/example/kristen/rottentomatoes/BoxOfficeMoviesAdapter.java
@@ -0,0 +1,44 @@
+package com.example.kristen.rottentomatoes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kristen on 8/8/15.
+ */
+public class BoxOfficeMoviesAdapter extends ArrayAdapter<BoxOfficeMovie> {
+    public BoxOfficeMoviesAdapter(Context context, ArrayList<BoxOfficeMovie> aMovies) {
+        super(context, 0, aMovies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        BoxOfficeMovie movie = getItem(position);
+
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_box_office_movie, parent, false);
+        }
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvCriticsScore = (TextView) convertView.findViewById(R.id.tvCriticsScore);
+        TextView tvCast = (TextView) convertView.findViewById(R.id.tvCast);
+        ImageView ivPosterImage = (ImageView) convertView.findViewById(R.id.ivPosterImage);
+
+        tvTitle.setText(movie.getTitle());
+        tvCriticsScore.setText("Score: " + movie.getCriticsScore() + "%");
+        tvCast.setText(movie.getCastList());
+        Picasso.with(getContext()).load(movie.getPosterUrl()).into(ivPosterImage);
+
+        return convertView;
+    }
+}
