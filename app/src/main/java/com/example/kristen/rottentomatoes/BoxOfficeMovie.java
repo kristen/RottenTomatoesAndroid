9a2fda87238152868c443943159c3c3f93dbd2e4
Kristen Sundquist <kssundquist@gmail.com>
2015-08-08 12:22:38 -0700

    Add BoxOfficeModel

---
+++ app/src/main/java/com/example/kristen/rottentomatoes/BoxOfficeMovie.java
@@ -0,0 +1,41 @@
+package com.example.kristen.rottentomatoes;
+
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kristen on 8/8/15.
+ */
+public class BoxOfficeMovie {
+    private String title;
+    private int year;
+    private String synopsis;
+    private String posterUrl;
+    private int criticsScore;
+    private ArrayList<String> castList;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public int getCriticsScore() {
+        return criticsScore;
+    }
+
+    public String getCastList() {
+        return TextUtils.join(", ", castList);
+    }
+}
